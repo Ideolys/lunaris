750513bcac016219a56417b0efe6d0f9c05930aa
Matthieu Robin <mrobin@ideolys.com>
2018-07-12 11:13:30 +0200

    fix vue plugin hooks

--- src/storePluginVue.js
+++ src/storePluginVue.js
@@ -15,6 +15,87 @@ lunaris._vue = {
   }),
   install : function (Vue, options) {
     /**
+     * Set $storeName value
+     * @param {String} store
+     * @param {Object} _this
+     */
+    function _setGet (store, _this) {
+      Object.defineProperty(_this, '$' + store, {
+        get : function () {
+          return lunaris._vue._vm.$data.$stores[store].state;
+        }
+      });
+    }
+
+    /**
+     * Set _get hook handler
+     * @param {String} store
+     */
+    function _get (store) {
+      return function (items) {
+        if (!Array.isArray(items)) {
+          return;
+        }
+
+        var _storeObj = lunaris._vue._vm.$data.$stores[store];
+        for (var i = 0; i < items.length; i++) {
+          _storeObj.state.push(items[i]);
+        }
+      }
+    }
+
+    /**
+     * Set _reset hook handler
+     * @param {String} store
+     */
+    function _reset (store) {
+      return function () {
+        lunaris._vue._vm.$data.$stores[store].state.splice(0);
+        lunaris.get('@' + store);
+      }
+    }
+
+    /**
+     * Set _insert hook handler
+     * @param {String} store
+     */
+    function _insert (store) {
+      return function (item) {
+        lunaris._vue._vm.$data.$stores[store].state.push(lunaris.clone(item));
+      }
+    }
+
+    /**
+     * Set _update hook handler
+     * @param {String} store
+     */
+    function _update (store) {
+      return function (item) {
+        var _state = lunaris._vue._vm.$data.$stores[store].state;
+        for (var j = 0; j < _state.length; j++) {
+          if (_state[j]._id === item._id) {
+            _state.splice(j, 1, lunaris.clone(item));
+          }
+        }
+      }
+    }
+
+    /**
+     * Set _delet hook handler
+     * @param {String} store
+     */
+    function _delete (store) {
+      return function (item) {
+        var _state = lunaris._vue._vm.$data.$stores[store].state;
+        for (var j = 0; j < _state.length; j++) {
+          if (_state._id === item._id) {
+            _state.splice(j, 1);
+          }
+        }
+      }
+    }
+
+    /**
      * Register stores to obtain vm.$<store>
      * @param {Object} _this instance vue
      */
@@ -37,51 +118,17 @@ lunaris._vue = {
         // re-initialize current store
         lunaris._vue._vm.$data.$stores[_store].state.splice(0);
 
-        Object.defineProperty(_this, '$' + _store, {
-          get : function () {
-            return lunaris._vue._vm.$data.$stores[_store].state;
-          }
-        });
+        _setGet(_store, _this);
 
         if (!_this.$options.storeHooks) {
           _this.$options.storeHooks = {};
         }
 
-        _this.$options.storeHooks['get@' + _store] = function (items) {
-          if (!Array.isArray(items)) {
-            return;
-          }
-
-          var _storeObj = lunaris._vue._vm.$data.$stores[_store];
-          for (var i = 0; i < items.length; i++) {
-            _storeObj.state.push(items[i]);
-          }
-        };
-
-        _this.$options.storeHooks['reset@' + _store] = function () {
-          lunaris._vue._vm.$data.$stores[_store].state.splice(0);
-          lunaris.get('@' + _store);
-        };
-
-        _this.$options.storeHooks['insert@' + _store] = function (item) {
-          lunaris._vue._vm.$data.$stores[_store].state.push(lunaris.clone(item));
-        };
-        _this.$options.storeHooks['update@' + _store] = function (item) {
-          var _state = lunaris._vue._vm.$data.$stores[_store].state;
-          for (var j = 0; j < _state.length; j++) {
-            if (_state[j]._id === item._id) {
-              _state.splice(j, 1, lunaris.clone(item));
-            }
-          }
-        };
-        _this.$options.storeHooks['delete@' + _store] = function (item) {
-          var _state = lunaris._vue._vm.$data.$stores[_store].state;
-          for (var j = 0; j < _state.length; j++) {
-            if (_state._id === item._id) {
-              _state.splice(j, 1);
-            }
-          }
-        };
+        _this.$options.storeHooks['get@'    + _store] = _get(_store);
+        _this.$options.storeHooks['reset@'  + _store] = _reset(_store);
+        _this.$options.storeHooks['insert@' + _store] = _insert(_store);
+        _this.$options.storeHooks['update@' + _store] = _update(_store);
+        _this.$options.storeHooks['delete@' + _store] = _delete(_store);
       }
     }
 
