0f192080aad22408e3bd4f13774c8e02ab7761e7
Matthieu Robin <mrobin@ideolys.com>
2019-04-10 14:23:02 +0200

    fix exports

--- karma.conf.js
+++ karma.conf.js
@@ -1,20 +1,20 @@
 // Karma configuration
 // Generated on Tue Jul 31 2018 10:27:23 GMT+0200 (CEST)
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    basePath : '',
 
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'should'],
+    frameworks : ['mocha', 'should'],
 
 
     // list of files / patterns to load in the browser
-    files: [
+    files : [
       'tests/client/before.js',
       'tests/client/testbuild.index.js',
       'tests/client/test.*.js'
@@ -22,50 +22,50 @@ module.exports = function(config) {
 
 
     // list of files / patterns to exclude
-    exclude: [
+    exclude : [
     ],
 
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
+    preprocessors : {
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters : ['progress'],
 
 
     // web server port
-    port: 9876,
+    port : 9876,
 
 
     // enable / disable colors in the output (reporters and logs)
-    colors: true,
+    colors : true,
 
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel : config.LOG_INFO,
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    autoWatch : true,
 
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [],
+    browsers : [],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun : false,
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
-}
+    concurrency : Infinity
+  });
+};
